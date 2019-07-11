f2ccfd91826e539278d036fe58959dfba2ffba38
lejiebianlidian <visualstudio365@gmail.com>
2019-07-11 14:19:30 +0800

    增加新实体

--- EFCoreAndMVC/Models/Student.cs
+++ EFCoreAndMVC/Models/Student.cs
@@ -7,5 +7,17 @@ namespace EFCoreAndMVC.Models
 {
     public class Student
     {
+        public int id { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+
+        public  MyProperty { get; set; }
+
+
+
     }
 }
